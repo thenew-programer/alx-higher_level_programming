5feb110d63df265ee435670fefc24fea2a2ca7ad
Youssef Bouryal <youssefbouryal02@gmail.com>
2023-08-10 23:44:20 +0100

    3rd program

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,10 +6,9 @@ if __name__ == "__main__":
     lenght = len(sys.argv) - 1
     args = sys.argv[1:]
     if (lenght == 0):
-        print("{} argument.".format(lenght))
-        sys.exit(1)
+        print("0 argument.")
     elif (lenght == 1):
-        print("{} argument:".format(lenght))
+        print("1 argument:")
     else:
         print("{} arguments:".format(lenght))
 
