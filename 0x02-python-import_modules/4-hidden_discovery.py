a9cfa6e0f6d8a647412223714811c54ad1203932
Youssef Bouryal <youssefbouryal02@gmail.com>
2023-08-11 18:59:50 +0100

    5th program

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import struct
-    with open("hidden_4.pyc", mode="rb") as file:
-        fileContent = file.read()
-        print(fileContent[20:-4])
+    import hidden_4
+    names = dir(hidden_4)
+    for name in names:
+        if name[:2] != "__":
+            print(name)
