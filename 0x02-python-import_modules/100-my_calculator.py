8f51960b33040f9384524a821ec8f98f3d1d5c43
Youssef Bouryal <youssefbouryal02@gmail.com>
2023-08-11 19:41:58 +0100

    100th program

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-import sys
-def main():
-    from my_calculator_1 import add, sub, mul, div
+if __name__ == "__main__":
+    import sys
+    from calculator_1 import add, sub, mul, div
     if len(sys.argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
@@ -20,9 +20,3 @@ def main():
         case _:
             print("Unknown operator. Available operators: +, -, * and /")
             sys.exit(1)
-            
-
-
-
-if __name__ == "__main__":
-    main()
