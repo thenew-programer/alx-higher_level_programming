18273a49f0325a1b4a8d4fcadc9a3ef1c5244ecb
Youssef Bouryal <youssefbouryal02@gmail.com>
2023-08-11 19:53:32 +0100

    100th program

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -2,14 +2,16 @@
 if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
     import sys
-    ops = {"+": add, "-":sub, "*":mul, "/":div}
-    if len(sys.argv) != 4:
+
+    if len(sys.argv) - 1 != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
+
+    ops = {"+": add, "-": sub, "*": mul, "/": div}
     if sys.argv[2] not in list(ops.keys()):
-        print("Unknown operator. Available operators: +, -, * and /")
+        print("unknown operation. available operations: +, -, * and /")
         sys.exit(1)
 
     a = int(sys.argv[1])
     b = int(sys.argv[3])
-    print("{} {} {} = {}".format(a, sys.argv[3], b, ops[sys.argv[2]](a, b)))
+    print("{} {} {} = {}".format(a, sys.argv[2], b, ops[sys.argv[2]](a, b)))
