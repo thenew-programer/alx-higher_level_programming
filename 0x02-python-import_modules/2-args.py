b52a73cfaa1b73428139118484e8c14179909e4b
Youssef Bouryal <youssefbouryal02@gmail.com>
2023-08-11 18:50:44 +0100

    3rd program

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,16 +1,13 @@
 #!/usr/bin/python3
-
 if __name__ == "__main__":
     import sys
 
-    lenght = len(sys.argv) - 1
-    args = sys.argv[1:]
-    if (lenght == 0):
-        print("0 argument.")
-    elif (lenght == 1):
+    count = len(sys.argv) - 1
+    if count == 0:
+        print("0 arguments.")
+    elif count == 1:
         print("1 argument:")
     else:
-        print("{} arguments:".format(lenght))
-
-    for i, arg in enumerate(args, start=1):
-        print("{}: {}".format(i, arg))
+        print("{} arguments:".format(count))
+    for i in range(count):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
