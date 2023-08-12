ddcc525751e288258b64786440cb3033f6b6312c
Youssef Bouryal <youssefbouryal02@gmail.com>
2023-08-12 23:46:20 +0100

    8th program

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    """adding 2 tuples."""
-    if len(tuple_a) < 2:
-        if len(tuple_a) == 0:
-            tuple_a = 0, 0
+    a = tuple_a.copy()
+    b = tuple_b.copy()
+    if len(a) < 2:
+        if len(a) == 0:
+            a = 0, 0
         else:
-            tuple_a = tuple_a[0], 0
-    elif len(tuple_b) < 2:
-        if len(tuple_b) == 0:
-            tuple_b = 0, 0
+            a = a[0], 0
+    elif len(b) < 2:
+        if len(b) == 0:
+            b = 0, 0
         else:
-            tuple_b = tuple_b[0], 0
+            b = b[0], 0
 
-    return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
+    return (a[0] + b[0], a[1] + b[1])
