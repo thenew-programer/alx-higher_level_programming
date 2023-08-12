e66e71001e365c5aed3277e15d99b8e85bf28872
Youssef Bouryal <youssefbouryal02@gmail.com>
2023-08-12 22:55:14 +0100

    11th program

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    new_list = my_list.copy()
-    for idx, num  in enumerate(my_list):
+    result = []
+    for num in my_list:
         if num % 2 == 0:
-            new_list[idx] = True
+            result.append(True)
         else:
-            new_list[idx] = False
-    return new_list
-
-print(divisible_by_2([10, 11, 12, 13, 15]))
+            result.append(False)
+    return result
