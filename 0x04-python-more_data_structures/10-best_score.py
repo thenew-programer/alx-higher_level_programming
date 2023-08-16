467ab9f14df9dafa1814cc3d735a909d3bc42a00
Youssef Bouryal <youssefbouryal02@gmail.com>
2023-08-17 00:35:52 +0100

    10th program

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     bs = 0
+    bs_key = ''
     for key in a_dictionary.keys():
         if a_dictionary[key] > bs:
             bs = a_dictionary[key]
-    return bs
+            bs_key = key
+    return bs_key
