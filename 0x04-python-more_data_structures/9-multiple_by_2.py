9a123df52d3af9bc327f782850a3828c4091a6f7
Youssef Bouryal <youssefbouryal02@gmail.com>
2023-08-17 00:29:20 +0100

    deleted

--- 0x04-python-more_data_structures/9-multiple_by_2.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-def multiply_by_2(a_dictionary):
-    dict_ = {1: 1}
-    dict_.pop(1)
-    for key in a_dictionary.keys():
-        dict_[key] = a_dictionary[key] * 2
-    return dict_
