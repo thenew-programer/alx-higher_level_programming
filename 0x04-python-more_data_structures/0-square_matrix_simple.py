4570e31f01451990ec3c35fa4b2c7ca1bc53e50c
Youssef Bouryal <youssefbouryal02@gmail.com>
2023-08-16 22:54:54 +0100

    0th program

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            matrix[i][j] **= 2
+    return matrix
