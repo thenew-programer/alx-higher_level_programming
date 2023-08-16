5056dbebd882b25e52903a53fed013d61fb354af
Youssef Bouryal <youssefbouryal02@gmail.com>
2023-08-16 23:35:14 +0100

    3rd program

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -6,7 +6,3 @@ def common_elements(set_1, set_2):
         if element in set_2:
             common_set.add(element)
     return common_set
-set_1 = { "Python", "C", "Javascript" }
-set_2 = { "Bash", "C", "Ruby", "Perl" }
-c_set = common_elements(set_1, set_2)
-print(sorted(list(c_set)))
