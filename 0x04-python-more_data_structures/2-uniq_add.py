1f64f4ec034a7d7ab6f955bc71ecb043ab681e50
Youssef Bouryal <youssefbouryal02@gmail.com>
2023-08-16 23:19:41 +0100

    2nd program

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -3,5 +3,5 @@ def uniq_add(my_list=[]):
     uniq_list = []
     for element in my_list:
         if element not in uniq_list:
-            uniq_add.append(element)
+            uniq_list.append(element)
     return uniq_list
