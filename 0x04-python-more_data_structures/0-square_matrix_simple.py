4a489bf3b1d5516538ffced9ecbc2ea1ca2df39d
Youssef Bouryal <youssefbouryal02@gmail.com>
2023-08-17 01:48:57 +0100

    0th program;

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,14 +1,8 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    new_matrix = [[], [], []]
+    new_matrix = []
     for i in range(len(matrix)):
+        new_matrix.append(matrix[i])
         for j in range(len(matrix[i])):
-            new_matrix[i].append(matrix[i][j] ** 2)
+            new_matrix[i][j] = matrix[i][j] ** 2
     return new_matrix
-matrix = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
-]
-print(square_matrix_simple(matrix))
-print(matrix)
