5aabd5ad7ad25279883997c1455e79b827dcd16c
Youssef Bouryal <youssefbouryal02@gmail.com>
2023-08-28 19:23:39 +0100

    1st program printing a list and catching indexError when the index is out of range

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
+
 def safe_print_list(my_list=[], x=0):
-    i = 0;
-    while (True):
+    for i in range(x):
         try:
             print(my_list[i], end="")
             i += 1
         except (IndexError):
+            x = i
             break
     print()
-    return x - i
+    return x
