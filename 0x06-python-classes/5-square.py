c2905177cb256182358a2c5ed55740c5e7a90262
Youssef Bouryal <youssefbouryal02@gmail.com>
2023-08-29 23:54:43 +0100

    sixth program: print the square using hash

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+""" Defines a class named Square that sets an instance attribute
+    to the value provided by the user based on some asumptions """
+
+
+class Square:
+    """ constructor - initialize a private instance attribute
+        args:
+            size (int): the private instance attribute
+    """
+    def __init__(self, size=0):
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    """
+    area - returns the area of the square
+    """
+
+    def area(self):
+        return self.__size ** 2
+    """
+    size - getter : gets the __size attribute
+    """
+    @property
+    def size(self):
+        return self.__size
+    """
+    size - setter : change the  __size attribute
+    args:
+        value (int): the value we want to set to our var
+    """
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+    def my_print(self):
+        if self.__size == 0:
+            print()
+        else:
+            for i in range(self.__size):
+                for j in range(self.__size):
+                    print("#", end="")
+                print()
