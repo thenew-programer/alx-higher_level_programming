9120e1f6ce78b94182c436d33afab4494552851b
Youssef Bouryal <youssefbouryal02@gmail.com>
2023-08-29 23:56:12 +0100

    sixth program: print the square using hash

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -41,7 +41,11 @@ class Square:
             raise ValueError("size must be >= 0")
         else:
             self.__size = value
+
     def my_print(self):
+        """
+        my_print - print square in hashes
+        """
         if self.__size == 0:
             print()
         else:
