d77140f44005883d5f3011f5069d8770b4a6b22f
Youssef Bouryal <youssefbouryal02@gmail.com>
2023-09-05 00:26:42 +0100

    second program declaring a real class of a rectangle that has two private instance attributes with there setters and getters

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""define a class representing a rectangle"""
+
+
+class Rectangle:
+    """ class that hold the width and height of a rectangle
+    :attribute width: (int)
+    :attribute height: (int)
+
+    :method width: getter -> int
+    :method width: setter -> void
+
+    :method height: getter -> int
+    :method height: setter -> void
+    """
+    def __init__(self, width=0, height=0):
+        if type(width) is not int:
+            raise TypeError("width must be an integer")
+        elif width < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = width
+
+        if type(height) is not int:
+            raise TypeError("heigth must be an integer")
+        elif height < 0:
+            raise ValueError("heigth must be >= 0")
+        else:
+            self.__height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        self.__height = value
