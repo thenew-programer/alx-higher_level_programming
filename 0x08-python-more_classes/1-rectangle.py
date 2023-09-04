a2d2927b7e6810af87f10b438fc961d747fad627
Youssef Bouryal <youssefbouryal02@gmail.com>
2023-09-05 00:29:47 +0100

    fixed error msg issues

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -18,14 +18,12 @@ class Rectangle:
             raise TypeError("width must be an integer")
         elif width < 0:
             raise ValueError("width must be >= 0")
-        else:
-            self.__width = width
-
-        if type(height) is not int:
+        elif type(height) is not int:
             raise TypeError("height must be an integer")
         elif height < 0:
             raise ValueError("height must be >= 0")
         else:
+            self.__width = width
             self.__height = height
 
     @property
