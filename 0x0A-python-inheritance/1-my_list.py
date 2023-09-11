c07fe097f490b927eb681a631c1947515ad7d939
Youssef Bouryal <youssefbouryal02@gmail.com>
2023-09-11 21:17:03 +0100

    1: class inherites from list class -  adds option to initialize the object with a predefined list

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-"""class MyList that inherites from list class"""
+"""Defines an inherited list class MyList."""
 
 
 class MyList(list):
-    def __init__(self, arr=[]):
-        super().__init__(arr)
+    """Implements sorted printing for the built-in list class."""
 
     def print_sorted(self):
+        """Print a list in sorted ascending order."""
         print(sorted(self))
