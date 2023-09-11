150434d919ec4687cab63c2002a19e9ba28b7a98
Youssef Bouryal <youssefbouryal02@gmail.com>
2023-09-11 19:07:34 +0100

    pycodestyle: issues

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -3,7 +3,7 @@
 
 
 def is_same_class(obj, a_class):
-    """return true if obj is an instance of a_class 
+    """return true if obj is an instance of a_class
     False otherwise"""
     if (type(obj) is a_class):
         return True
