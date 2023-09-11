90404094ea4fc96d017e7dff0cdcb2c5e0bfbcfd
Youssef Bouryal <youssefbouryal02@gmail.com>
2023-09-11 21:16:28 +0100

    7: add function Public instance method: def integer_validator(self, name, value): that validates value

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -1,38 +1,25 @@
 #!/usr/bin/python3
-"""
-class BaseGeometry
-"""
+"""Defines a base geometry class BaseGeometry."""
 
 
 class BaseGeometry:
-    """
-    Represent base geometry
+    """Reprsent base geometry."""
 
-    method:
-        area(void) -> nothing: raise an Exeption
-
-        integer_validator(name: str, value: any) -> nothing: raise TypeError and ValueError
-        based on the type of value
-    """
     def area(self):
-        """
-        unimplemented
-        """
+        """Not yet implemented."""
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        """
-         validates value
+        """Validate a parameter as an integer.
 
-         Args:
+        Args:
             name (str): The name of the parameter.
             value (int): The parameter to validate.
         Raises:
             TypeError: If value is not an integer.
             ValueError: If value is <= 0.
         """
-        if type(value) is not int:
+        if type(value) != int:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
-
