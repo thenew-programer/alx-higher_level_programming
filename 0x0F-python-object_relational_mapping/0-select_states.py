ae82ae58be586fbc0540ff58c8d37dcf8a1e3b28
Youssef Bouryal <youssefbouryal02@gmail.com>
2023-11-21 00:00:14 +0100

    select all from a database

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -5,7 +5,7 @@ script to Lists all states from the database hbtn_0e_0_usa
 import sys
 import MySQLdb
 
-def main():
+if __name__ == '__main__':
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
                          db=sys.argv[3], port=3306)
 
@@ -15,6 +15,3 @@ def main():
 
     for state in states:
         print(state)
-
-if __name__ == '__main__':
-    main()
