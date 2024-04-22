5da5e17f5189433e6f3b806271aaff7474d16164
Youssef Bouryal <youssefbouryal02@gmail.com>
2024-04-22 19:17:42 +0100

    9-add.js

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -3,4 +3,4 @@ function add (a, b) {
   return (a + b);
 }
 
-if (process.argv <= 3 || isNaN(parseInt(process.argv[2])) || isNaN(parseInt(process.argv[3]))) { console.log(NaN); } else { console.log(add(parseInt(process.argv[2]), parseInt(process.argv[2]))); }
+if (process.argv <= 3 || isNaN(parseInt(process.argv[2])) || isNaN(parseInt(process.argv[3]))) { console.log(NaN); } else { console.log(add(Number(process.argv[2]), Number(process.argv[2]))); }
