16735cf6fce2086351168764b627ee0c985831c7
Youssef Bouryal <youssefbouryal02@gmail.com>
2024-04-22 20:00:28 +0100

    11-second_biggest.js

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,8 +1,9 @@
 #!/usr/bin/node
 if (process.argv.length <= 3) console.log(0);
 else {
-  const args = process.argv.map(Number)
+  const listlen = process.argv.length - 2;
+  const list = process.argv.map(Number)
     .slice(2, process.argv.length)
     .sort((a, b) => a - b);
-  console.log(args[process.argv.length - 4]);
+  console.log(list[listlen - 2]);
 }
