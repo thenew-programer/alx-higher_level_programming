f5b7547d7205e504ead3307a52869a6189bae81a
Youssef Bouryal <youssefbouryal02@gmail.com>
2024-04-22 17:44:29 +0100

    2-argument.js

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,7 +2,7 @@
 const process = require('process');
 
 if (process.argv.length > 2) {
-  console.log("Argument found");
+  console.log('Argument found');
 } else {
-  console.log("No argument");
+  console.log('No argument');
 }
