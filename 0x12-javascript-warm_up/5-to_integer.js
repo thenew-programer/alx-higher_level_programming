9ab7d14119ce2b7467e50a2bfcc4d86ddd7022a2
Youssef Bouryal <youssefbouryal02@gmail.com>
2024-04-22 18:59:39 +0100

    5-to_integer.js

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+if (process.argv.length <= 2 || isNaN(parseInt(process.argv[2]))) {
+  console.log('Not a number');
+} else {
+  console.log('My number: ' + parseInt(process.argv[2]));
+}
