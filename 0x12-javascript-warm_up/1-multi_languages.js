6dfed4ad7e19440ceb6ac9edd518c9e0b660f3c1
Youssef Bouryal <youssefbouryal02@gmail.com>
2024-04-22 17:15:01 +0100

    1-multi_languages.js

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,3 +1,4 @@
-console.log("C is fun")
-console.log("Python is cool")
-console.log("JavaScript is amazing")
+#!/usr/bin/node
+console.log('C is fun')
+console.log('Python is cool')
+console.log('JavaScript is amazing')
