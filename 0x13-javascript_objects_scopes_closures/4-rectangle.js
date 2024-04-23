fecf17b6206dac6b83e9c622ccf0f549db7f41c3
Youssef Bouryal <youssefbouryal02@gmail.com>
2024-04-23 08:24:05 +0100

    4-rectangle.js

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -7,9 +7,9 @@ module.exports = class Rectangle {
     }
   }
 
-  print () {
+  print (char = 'X') {
     for (let i = 0; i < this.height; i++) {
-      console.log('X'.repeat(this.width));
+      console.log(char.repeat(this.width));
     }
   }
 
