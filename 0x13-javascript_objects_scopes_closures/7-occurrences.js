39c3e2b7699753cd3d432cd85ba4ebea99f3f59d
Youssef Bouryal <youssefbouryal02@gmail.com>
2024-04-23 19:55:30 +0100

    7-occurrences.js

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,6 +1,5 @@
 #!/usr/bin/node
 exports.nbOccurences = function (list, searchElement) {
   let occurences = 0;
-  list.array.forEach((element) => element === searchElement ? occurences++ : '');
-  return occurences;
+  return list.array.forEach((element) => element === searchElement ? occurences++ : occurences);
 };
