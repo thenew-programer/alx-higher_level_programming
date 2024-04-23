26c437a6898b1d2c496d02f16a991a29643c0f22
Youssef Bouryal <youssefbouryal02@gmail.com>
2024-04-23 19:45:41 +0100

    6-square.js

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,6 +1,9 @@
 #!/usr/bin/node
 module.exports = class Square extends require('./5-rectangle.js') {
-  charPrint (c = 'X') {
-    this.print(c);
+  charPrint (c) {
+    if (c === undefined)
+      this.print(c);
+    else
+      for (let i = 0; i < this.length; i++) console.log(c.repeat(this.width));
   }
 };
