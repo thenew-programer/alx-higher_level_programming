68c84f98a2e4a5b887cdec6108dfb6a38c79509d
Youssef Bouryal <youssefbouryal02@gmail.com>
2024-04-23 08:26:12 +0100

    6-square.js

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+module.exports = class Square extends require('./4-rectangle.js') {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (char = 'X') {
+    this.print(char);
+  }
+};
