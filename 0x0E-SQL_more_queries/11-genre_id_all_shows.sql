41d46bf927f2a415b573bb1c1d2c7d8d400b7736
Youssef Bouryal <youssefbouryal02@gmail.com>
2024-04-24 20:10:16 +0100

    left join

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,8 +1,7 @@
 -- lists all shows contained in the database hbtn_0d_tvshows.
-SELECT S.title,
-    CASE WHEN  G.genre_id <> NULL THEN G.genre_id ELSE NULL
-    FROM tv_shows as S
-    LEFT JOIN tv_show_genres as G
-    ON S.id = G.show_id
-    ORDER BY S.title, G.genre_id;
+SELECT S.title, G.genre_id
+FROM tv_shows as S
+LEFT JOIN tv_show_genres as G
+ON S.id = G.show_id
+ORDER BY S.title, G.genre_id;
 
