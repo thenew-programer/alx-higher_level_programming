a8f0a789a8a26ee1883d1225ba55ec263e58627f
Youssef Bouryal <youssefbouryal02@gmail.com>
2024-04-24 20:12:13 +0100

    right join

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,7 +1,7 @@
 -- lists all shows contained in the database hbtn_0d_tvshows.
 SELECT S.title, G.genre_id
-FROM tv_shows as S
-LEFT JOIN tv_show_genres as G
-ON S.id = G.show_id
-ORDER BY S.title, G.genre_id;
-
+    FROM tv_show_genres as G
+    RIGHT JOIN tv_shows as S
+    ON G.show_id = S.id
+    ORDER BY S.title, G.genre_id
+;
