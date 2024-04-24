842f7a97c24bb8e1b9bb13f3982844a93d244085
Youssef Bouryal <youssefbouryal02@gmail.com>
2024-04-24 20:19:53 +0100

    full outer join

---
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,8 @@
+--  lists all shows contained in hbtn_0d_tvshows without a genre linked
+SELECT S.title, G.genre_id
+    FROM tv_shows AS S
+    FULL JOIN tv_show_genres AS G
+    ON S.id = G.show_id
+    WHERE G.show_id IS NULL
+    ORDER BY S.title, G.genre_id
+;
