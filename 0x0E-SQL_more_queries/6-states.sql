187f53f8261c6cba69881ebd659cb3ddf04c3277
Youssef Bouryal <youssefbouryal02@gmail.com>
2024-04-24 11:33:06 +0100

    create a db and a table with col that have specific constraints

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,9 @@
+-- creates the database hbtn_0d_usa and the table states
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
+-- CREATE table states in hbtn_0d_usa
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states (
+  id INT PRIMARY AUTO_INCREMENT,
+  name VARCHAR(256) NOT NULL
+);
