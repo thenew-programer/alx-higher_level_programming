a51bda95292c4cd393ce03e440c8287c6f003d1c
Youssef Bouryal <youssefbouryal02@gmail.com>
2024-04-24 20:02:27 +0100

    FULL join

---
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,7 @@
+-- lists all shows contained in the database hbtn_0d_tvshows.
+SELECT S.title, G.genre_id
+FROM tv_shows as S
+FULL JOIN tv_show_genres as G
+ON S.id = G.show_id
+ORDER BY S.title, G.genre_id;
+
