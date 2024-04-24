be23aaa33ff8ce4eab5bb72d2b8a2165afd0f9c1
Youssef Bouryal <youssefbouryal02@gmail.com>
2024-04-24 20:25:44 +0100

    full outer join

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -3,6 +3,7 @@ SELECT S.title, G.genre_id
     FROM tv_shows AS S
     FULL JOIN tv_show_genres AS G
     ON S.id = G.show_id
-    WHERE G.show_id IS NULL
+    WHERE S.id IS NULL
+    OR G.show_id IS NULL
     ORDER BY S.title, G.genre_id
 ;
