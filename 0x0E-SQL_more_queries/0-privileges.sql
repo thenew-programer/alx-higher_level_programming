3ef381ffcb61eaf1645f50176012ececa474b438
Youssef Bouryal <youssefbouryal02@gmail.com>
2024-04-24 11:00:36 +0100

    show privileges of users

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,4 +1,4 @@
 -- lists all privileges of the MySQL users user_0d_1
-SHOW GRANT FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
 -- lists all privileges of the MySQL users user_0d_2
-SHOW GRANT FOR 'user_0d_2'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
