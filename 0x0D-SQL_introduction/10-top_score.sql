3a3e0aad0be5b70e3e5f236d6c9b8c4894f6960c
Youssef Bouryal <youssefbouryal02@gmail.com>
2024-04-24 08:55:15 +0100

    list records with the format (score name) ordered by score descendent

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,5 +1,4 @@
--- Display records with this format (score name) in
--- descending order
-SELECT (score, name)
+-- Display records with this format (score name) in descendent order
+SELECT score, name
   FROM second_table
   ORDER BY score DESC;
