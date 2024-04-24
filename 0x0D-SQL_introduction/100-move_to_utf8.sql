f4d663a63701187d8ae2ed21c8134aba2875915f
Youssef Bouryal <youssefbouryal02@gmail.com>
2024-04-24 09:42:40 +0100

    converts hbtn_0c_0 database to UTF8 (utf8mb4, collate utf8mb4_unicode_ci)

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,7 +1,7 @@
--- converts hbtn_0c_0 database to UTF8 
+-- converts hbtn_0c_0 database to UTF8
 ALTER DATABASE hbtn_0c_0
     CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
--- converts first_table table to UTF8 
+-- converts first_table table to UTF8
 USE hbtn_0c_0;
 ALTER TABLE first_table
-    CONVERT TO CHARACTER SET uft8mb4 COLLATE utf8mb4_unicode_ci;
+    CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
