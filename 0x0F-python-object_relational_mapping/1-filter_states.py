8f96d7753abddae095892050c7a30bf49d5f17f3
Youssef Bouryal <youssefbouryal02@gmail.com>
2024-04-25 10:20:13 +0100

    1-filter states using ORM

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+lists all states with a name starting with N
+(upper N) from the database hbtn_0e_0_usa
+"""
+
+from sys import argv
+import MySQLdb
+
+if __name__ == "__main__":
+    """
+    connect to db and run the sql query
+    """
+    conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                           passwd=argv[2], db=argv[3], charset="utf8")
+    cur = conn.cursor()
+    # HERE I have to know SQL to grab all states in my database
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%'")
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
