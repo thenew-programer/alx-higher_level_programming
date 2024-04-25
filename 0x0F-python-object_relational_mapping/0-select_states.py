4eb421dfe31716a9db2aab3326a6c49f477aebf6
Youssef Bouryal <youssefbouryal02@gmail.com>
2024-04-25 10:02:55 +0100

    select all using ORM in py

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,24 +1,21 @@
 #!/usr/bin/python3
 '''Select sql statement using ORM'''
-import sys
+
+from sys import argv
 import MySQLdb
 
 if __name__ == "__main__":
-    if len(sys.argv) < 2:
-        print(len(sys.argv))
-    else:
-        print(len(sys.argv))
-        username = sys.argv[1]
-        password = sys.argv[2]
-        db_name = sys.argv[3]
-
-        conn = MySQLdb.connect(host="localhost", port=3306, user=username,
-                               passwd=password, db=db_name, charset="utf8")
-        cur = conn.cursor()
-        # HERE I have to know SQL to grab all states in my database
-        cur.execute("SELECT * FROM states ORDER BY id ASC")
-        query_rows = cur.fetchall()
-        for row in query_rows:
-            print(f'({row.id}, {row.name})')
-        cur.close()
-        conn.close()
+    """
+    Access to the database and get the states
+    from the database.
+    """
+    conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                           passwd=argv[2], db=argv[3], charset="utf8")
+    cur = conn.cursor()
+    # HERE I have to know SQL to grab all states in my database
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
