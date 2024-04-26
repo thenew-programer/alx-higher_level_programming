15389f1ee007e471cc4e7ef530e12732f97bd378
Youssef Bouryal <youssefbouryal02@gmail.com>
2024-04-26 10:50:10 +0100

    select all records from cities table

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -21,7 +21,14 @@ if __name__ == "__main__":
     conn = MySQLdb.connect(host='localhost', port=3306, user=argv[1],
                            passwd=argv[2], db=argv[3])
     cur = conn.curser()
-    cur.execute("SELECT * FROM cities ORDER BY states.id")
+    cur.execute("""
+                SELECT
+                    cities.id, cities.name, states.name
+                FROM
+                    cities
+                JOIN
+                    states ON cities.state_id = states.id
+                """)
     rows = cur.fetchall()
     for row in rows:
         print(row)
