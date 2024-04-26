334838545abf5de29e49afbe02a8b4b1f1bc4ca7
Youssef Bouryal <youssefbouryal02@gmail.com>
2024-04-26 09:12:02 +0100

    filter states using argument given by the user

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -8,13 +8,12 @@ from sys import argv
 import MySQLdb
 
 if __name__ == "__main__":
+    query = "SELECT * FROM states \
+            WHERE name = '{}' \
+            ORDER BY states.id".format(argv[4])
     conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
                            passwd=argv[2], db=argv[3])
     cur = conn.cursor()
-    # HERE I have to know SQL to grab all states in my database
-    query = "SELECT * FROM states \
-            WHERE name = {} \
-            ORDER BY states.id".format(argv[4])
     cur.execute(query)
     query_rows = cur.fetchall()
     for row in query_rows:
