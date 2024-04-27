b1a542468980618e91fd5d4bd328ab0d52bb0b6e
Youssef Bouryal <youssefbouryal02@gmail.com>
2024-04-27 11:45:23 +0100

    fetch all from states table using sqlalchemy

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -13,11 +13,11 @@ if __name__ == "__main__":
     fetch all records from states table
     '''
 
-    uri = 'mysql+mysqldb://{0}:{1}@localhost:3306/{2}'.format(
+    uri = 'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
         argv[1], argv[2], argv[3])
     engine = create_engine(uri)
-
     Session = sessionmaker(bind=engine)
+
     session = Session()
     for state in session.query(State).order_by(State.id):
         print("{}: {}".format(state.id, state.name))
