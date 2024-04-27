bce6c2e3cc4e9dcfa662b3fbf1b5bd5c7535282c
Youssef Bouryal <youssefbouryal02@gmail.com>
2024-04-27 11:11:23 +0100

    model state table using sqlalchemy

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,21 @@
+'''
+lass definition of a State and an instance Base = declarative_base()
+'''
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class State(Base):
+    '''
+    State class inherits from SQLalchemy Base Class
+    Attr:
+        __tablename__(str): table name
+        id (int): state table column
+        name (str): state table column
+    '''
+    __tablename__ = 'states'
+    id = Column('id', Integer, nullable=False,
+                primary_key=True, unique=True, autoincrement=True)
+    name = Column('name', String(128), nullable=False)
