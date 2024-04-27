dda74e303ff90b5071e7d6aad1f349cfcb115df3
Youssef Bouryal <youssefbouryal02@gmail.com>
2024-04-27 13:17:24 +0100

    model state filter using input from user

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -17,7 +17,8 @@ if __name__ == "__main__":
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    instance = session.query(State).filter(State.name == (sys.argv[4], )).first()
+    instance = session.query(State).filter(
+        State.name == (sys.argv[4], )).first()
     if instance:
         print(instance.id)
     else:
