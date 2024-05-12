b1e608961d8c9332c22699f972fd1e5f6c8dace8
Youssef Bouryal <youssefbouryal02@gmail.com>
2024-05-12 11:44:11 +0100

    0-body_size.sh

---
+++ 0x10-python-network_0/0-body_size.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+# script that takes in a URL, sends a request
+# to that URL, and displays the size of the
+# body of the response
+
+if [ "$#" -lt 1 ];
+then
+    exit;
+else
+    curl -s http://"$1" | wc -c
+fi;
