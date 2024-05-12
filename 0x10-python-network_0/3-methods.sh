77903051e4de42bf86d0c444badf91f3c8c6d1d6
Youssef Bouryal <youssefbouryal02@gmail.com>
2024-05-12 13:08:02 +0100

    3-methods.sh

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # script that takes in a URL and displays
 # all HTTP methods the server will accept
-curl -Is "$1" | grep -i "allow:" | cut -d ":" -f 2 | cut -c 2- | rev | cut -c 2- | rev
+curl -Is "$1" | awk '/Allow:/ {print substr($0, index($0, $2))}'
